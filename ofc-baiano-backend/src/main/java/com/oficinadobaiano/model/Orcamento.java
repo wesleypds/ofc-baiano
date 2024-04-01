e323c5e24744287d85304630e5a929ccbdd3c5c5
Wesley Pereira <wlypereiraa@gmail.com>
2024-04-01 13:07:13 -0300

    wip: add outras classes

--- ofc-baiano-backend/src/main/java/com/oficinadobaiano/model/Orcamento.java
+++ ofc-baiano-backend/src/main/java/com/oficinadobaiano/model/Orcamento.java
@@ -1,19 +1,28 @@
 package com.oficinadobaiano.model;
 
+import java.util.Date;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
 @Entity
-public class Produto {
+public class Orcamento {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    private String nome;
-    private String codigo;
-    private Integer quantidade;
+
+    private Date dataEntrada;
+
     private Double valor;
+
+    private Integer descontos;
+
+    @OneToMany(mappedBy = "orcamento")
+    private List<OrcamentoProduto> produtoOrcamentos;
 }
--- ofc-baiano-backend/src/main/java/com/oficinadobaiano/model/OrcamentoProduto.java
+++ ofc-baiano-backend/src/main/java/com/oficinadobaiano/model/OrcamentoProduto.java
@@ -4,16 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
-public class Produto {
+public class OrcamentoProduto {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    private String nome;
-    private String codigo;
-    private Integer quantidade;
-    private Double valor;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produto")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produto")
+    private Orcamento orcamento;
+
+    private int quantidade;
 }
