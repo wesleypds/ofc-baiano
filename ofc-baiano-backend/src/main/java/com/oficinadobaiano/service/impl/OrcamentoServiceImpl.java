6bf27627aa4164fb8932b5f45d0ef01ea4b4fac0
Wesley Pereira <wlypereiraa@gmail.com>
2024-04-29 17:22:36 -0300

    feat: adicionando validações nos processos

--- ofc-baiano-backend/src/main/java/com/oficinadobaiano/service/impl/OrcamentoServiceImpl.java
+++ ofc-baiano-backend/src/main/java/com/oficinadobaiano/service/impl/OrcamentoServiceImpl.java
@@ -5,8 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.oficinadobaiano.enums.EscolhaCliente;
 import com.oficinadobaiano.model.Orcamento;
+import com.oficinadobaiano.model.OrcamentoProduto;
+import com.oficinadobaiano.model.OrcamentoServico;
+import com.oficinadobaiano.model.PreOrcamento;
+import com.oficinadobaiano.model.Produto;
+import com.oficinadobaiano.model.Servico;
 import com.oficinadobaiano.repository.OrcamentoRepository;
+import com.oficinadobaiano.repository.PreOrcamentoRepository;
+import com.oficinadobaiano.repository.ProdutoRepository;
+import com.oficinadobaiano.repository.ServicoRepository;
 import com.oficinadobaiano.service.OrcamentoService;
 
 @Service
@@ -14,8 +23,24 @@ public class OrcamentoServiceImpl implements OrcamentoService {
     @Autowired
     private OrcamentoRepository orcamentoRepository;
 
+    @Autowired
+    private PreOrcamentoRepository preOrcamentoRepository;
+
+    @Autowired
+    private ServicoRepository servicoRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @Override
     public Orcamento save(Orcamento orcamento) {
+        Optional<PreOrcamento> db = preOrcamentoRepository.findById(orcamento.getPreOrcamento().getId());
+        PreOrcamento preOrcamento = db.get();
+        if (preOrcamento.getEscolha().equals(EscolhaCliente.ORCAMENTO_E_SERVICO) || preOrcamento.getEscolha().equals(EscolhaCliente.SERVICO)) {
+            orcamento.setProblemaCliente(preOrcamento.getProblema());
+        }
+        orcamento.setProblemaMecanico(null);
+        orcamento.setValor(calculaValorOrcamento(orcamento));
         return orcamentoRepository.save(orcamento);
     }
 
@@ -31,11 +56,49 @@ public class OrcamentoServiceImpl implements OrcamentoService {
 
     @Override
     public Orcamento update(Orcamento orcamento) {
+        orcamento.setValor(calculaValorOrcamento(orcamento));
         return orcamentoRepository.save(orcamento);
     }
 
     @Override
     public void remove(Long id) {
         orcamentoRepository.deleteById(id);
+    }
+
+    private Double calculaValorOrcamento(Orcamento orcamento) {
+        Double valor = 0.0;
+        Optional<Orcamento> dbOrcamento;
+        Orcamento orc;
+
+        if (orcamento.getId() != null) {
+            dbOrcamento = orcamentoRepository.findById(orcamento.getId());
+            orc = dbOrcamento.get();
+            if (orc != null) {
+                valor = orc.getValor();
+            }
+        }
+
+        if (orcamento.getServicos() != null) {
+            for (OrcamentoServico orcamentoServico : orcamento.getServicos()) {
+                if (orcamentoServico.getId() == null) {
+                    Optional<Servico> db = servicoRepository.findById(orcamentoServico.getServico().getId());
+                    Servico servico = db.get();
+                    valor += servico.getValor();
+                }
+            }
+        }
+
+        if (orcamento.getProdutoOrcamentos() != null) {
+            for (OrcamentoProduto orcamentoProduto : orcamento.getProdutoOrcamentos()) {
+                if (orcamentoProduto.getId() == null) {
+                    Optional<Produto> db = produtoRepository.findById(orcamentoProduto.getProduto().getId());
+                    Produto produto = db.get();
+                    Double valorTotalProduto = produto.getValor() * orcamentoProduto.getQuantidade();
+                    valor += valorTotalProduto;
+                }
+            }
+        }
+
+        return valor;
     }
 }
