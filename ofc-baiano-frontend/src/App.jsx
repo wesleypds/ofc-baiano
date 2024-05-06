c9b4438c83b9f08faa4a800c381e4e18c4622c9c
Alessandro Carvalho Santos <santoscarvalhoalessandro@gmail.com>
2024-05-06 15:04:43 -0300

    Correção de arquivos

--- ofc-baiano-frontend/src/App.jsx
+++ ofc-baiano-frontend/src/App.jsx
@@ -3,17 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./module/login/Login.jsx"
 import Home from "./module/home/Home.jsx"
 import Produtos from "./module/produto/Produtos.jsx"
+import Produto from "./module/produto/Produto.jsx"
+
 import Servicos from "./module/servico/Servicos.jsx"
-<<<<<<< HEAD
-=======
 import Usuarios from "./module/usuario/Usuarios.jsx"
->>>>>>> 2b652d2594f508236b282f3daa2a2b71f4cc06a3
-
-
-// import Home from "./containers/home/Home.jsx"
-// import Login from "./containers/login/Login.jsx"
-// import PacienteHome from "./containers/paciente/PacienteHome.jsx"
-// import PacienteCadastro from "./containers/paciente/PacienteCadastro.jsx"
 
 function App() {
   return(
@@ -24,14 +17,9 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/home" element={<Home/>}/>
           <Route path="/produtos" element={<Produtos/>}/>
-<<<<<<< HEAD
-          <Route path="/servicos" element={<Servicos/>}/>
-
-=======
-          <Route path="/produto" element={<Produtos/>}/>
+          <Route path="/produto" element={<Produto/>}/>
           <Route path="/servicos" element={<Servicos/>}/>
           <Route path="/usuarios" element={<Usuarios/>}/>
->>>>>>> 2b652d2594f508236b282f3daa2a2b71f4cc06a3
         </Routes>
       </BrowserRouter>
     </div>
