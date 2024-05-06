92ae7599fe16cf5f3fc3edf035caf0cf97de1745
joaovictormc <jvictormacielc@hotmail.com>
2024-05-06 15:20:03 -0300

    alteração produto

--- ofc-baiano-frontend/src/module/produto/Produtos.jsx
+++ ofc-baiano-frontend/src/module/produto/Produtos.jsx
@@ -3,11 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 import LayoutBase from "../../components/layout/LayoutBase.jsx"
+import { Button } from 'antd';
 
 const Produtos = () => {
 
   const locationUrl = useLocation();
   const navigate = useNavigate();
+  const redirect = () => {
+    const token  = locationUrl.state.token;
+    const userInfo = locationUrl.state.userInfo;
+    console.log(locationUrl)
+    navigate('/produto', { state: { token,  userInfo }});
+  }
 
   useEffect(() => {
     if (locationUrl.state.token != "7f08f0ae81840a4a1887d3bdf9201efb") {
@@ -19,7 +26,7 @@ const Produtos = () => {
     <LayoutBase userInfo={locationUrl.state.userInfo}>
       <h1><b>GRID Produtos</b></h1>
 
-     
+     <Button onClick={redirect}>teste</Button>
     </LayoutBase>
   );
 };
